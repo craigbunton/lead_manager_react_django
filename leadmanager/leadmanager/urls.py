af0bd51f43b43ebbec16c18596b1aa2cc7ff1b85
Craig Bunton <craigbunton@hotmail.com>
2019-11-01 00:12:15 +0000

    Initial Commit

---
+++ leadmanager/leadmanager/urls.py
@@ -0,0 +1,10 @@
+# Project Root URLS file
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
+    path('', include('leads.urls'))
+]
---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,9 @@
+# Leads app URLS file
+
+from rest_framework import routers
+from .api import LeadViewSet
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, 'leads')
+
+urlpatterns = router.urls
